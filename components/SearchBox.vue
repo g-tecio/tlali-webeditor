913c6aa76fb22871ef0ddaca515091848bb5f062
JaimeRdx <45768684+JaimeRdx@users.noreply.github.com>
2019-01-22 14:34:44 -0600

    Added search box

---
+++ components/SearchBox.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <input type="text" placeholder="Buscar..." v-model="searchTerm">
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      searchTerm: ''
+    }
+  },
+  watch: {
+    searchTerm: function() {
+      this.$emit('searchRecords', this.searchTerm);
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+div {
+  margin: 10px;
+}
+
+input {
+  padding: 0 10px 0 10px;
+  outline: none;
+  border: none;
+  background-color: #eee;
+  font-size: 20px;
+  margin: auto;
+  display: block;
+  height: 50px;
+  width: 60%;
+  min-width: 200px;
+}
+</style>
--- pages/_articles/index.vue
+++ pages/_articles/index.vue
@@ -1,29 +1,64 @@
 <template>
   <section class="articles">
     <h1> {{ $route.params.articles }} </h1>
-    <!-- Render and <Article/> component for each article fetched from articlesData -->
+
+      <!-- Whenever searchRecords is emmited from the child component SearchBox,
+      execute searchArticles from this index.vue component-->
+
+    <SearchBox
+      v-on:searchRecords="searchArticles"/>
+
     <hr>
+
     <Article
-      v-for="article in articlesData"
+      v-for="article in articlesList"
       v-bind:key="article.ID"
       v-bind:id="article.ID"
       v-bind:author="article.ARTICLE.Author"
       v-bind:title="article.ARTICLE.Title"
       v-bind:date="article.ARTICLE.DT"
-      v-bind:status="article.ARTICLE.PublishStatus"
-    />
+      v-bind:status="article.ARTICLE.PublishStatus" />
   </section>
 </template>
 
 <script>
-import Article from "@/components/Article"; //Get the Article component
+import Article from "@/components/Article";
+import SearchBox from "@/components/SearchBox";
 
 export default {
-  components: {
-    Article //Declare components to use in this document/page
+  name: 'MainApp',
+
+  data: function() {
+    return {
+      searchTerms: ''
+    }
   },
-  // Filter the articles, according to the Route.params and calling the right URL
-  async asyncData({ $axios, params }) {
+
+  components: {
+    Article,
+    SearchBox
+  },
+
+  methods: {
+    searchArticles: function(terms) {
+      this.searchTerms = terms;
+    }
+  },
+
+  computed: {
+    // Return articleList according to the search terms from the search box.
+    // If terms is empty, then return full list of articles.
+    articlesList: function() {
+      return this.articlesData.filter(function(item) {
+        return ( //If any of the following matchs returns true, the article is added to articleList
+          (item.ARTICLE.Title.toLowerCase().match(this.searchTerms.toLowerCase())) || //Search on the Title
+          (item.ARTICLE.Author.toLowerCase().match(this.searchTerms.toLowerCase())) //Search on the Author
+        )
+      }.bind(this));
+    }
+  },
+
+async asyncData({ $axios, params }) {
     if(params.articles == "Articulos") {
       const articlesData = await $axios.$get(
         "https://o2dstvq9sb.execute-api.us-west-2.amazonaws.com/dev/articles/");
